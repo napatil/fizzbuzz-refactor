a2d070b86b72757ec6bd2888677337cbcd613aac
napatil <namy10.05@gmail.com>
2015-10-25 18:23:13 -0700

    initial commit

---
+++ refactor.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	var aNumber = Number(window.prompt("Please enter a number"));
+	if (isNaN(aNumber)) {
+    	alert("Please use a number.");
+  	} 
+  	else if (aNumber%1 != 0) {
+    	alert("Please use a whole number."); 
+  	}
+  	else {
+     	fizzbuzz(aNumber);
+  	}
+});
+
+function fizzbuzz(max) {
+	var myBody = $("body");
+	for (var i = 1; i <= max; i++) {
+		if (i%3 === 0 && i%5 === 0) {
+			myBody.append(" FizzBuzz" + "<br>");
+		}
+		else if (i%3 === 0) {
+			myBody.append("Fizz" + "<br>");
+		}	
+		else if (i%5 === 0) {
+			myBody.append("Buzz" + "<br>");
+		}	
+		else {
+			myBody.append(i + "<br>");
+		}
+	}
+}
